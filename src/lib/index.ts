783328fd876d75502852fc8b2d1a333ec68f4f9b
fforres <felipe.torressepulveda@gmail.com>
2020-06-19 09:47:43 -0700

    remove?

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -22,12 +22,18 @@ export const getPokemonById = (pokemonId: Number): PokemonType | null => {
   };
 };
 
-export const getPokemonsByNames = (pokemonName: string): PokemonType[] => {
-  const pokemons = Pokedex.filter(pokemon => {
-    return pokemon.name.english
-      .toLowerCase()
-      .includes(pokemonName.toLowerCase());
-  }).map(pokemon => {
+export const getPokemonsByNames = (pokemonName: string = ''): PokemonType[] => {
+  const normalizedPokemonName = pokemonName.trim().toLowerCase();
+  const pokemons =
+    normalizedPokemonName === ''
+      ? Pokedex
+      : Pokedex.filter(pokemon => {
+          return pokemon.name.english
+            .toLowerCase()
+            .includes(normalizedPokemonName.toLowerCase());
+        });
+
+  return pokemons.map(pokemon => {
     return {
       id: pokemon.id,
       stats: pokemon.base,
@@ -39,5 +45,4 @@ export const getPokemonsByNames = (pokemonName: string): PokemonType[] => {
       type: pokemon.type
     };
   });
-  return pokemons;
 };
